b02e85d57f05fd8b4bf756b1ef56d6cb9d86999b
datdta95 <datdta95@gmail.com>
2018-08-15 14:48:04 +0700

    Pass với value là 30-4-2018

--- src/main/java/NextDayCalculator.java
+++ src/main/java/NextDayCalculator.java
@@ -4,10 +4,21 @@ public class NextDayCalculator {
     }
 
     public static int[] nextDay(int[] date) {
-        if (date[0] == 31) {
-            date[0] = 1;
-            date[1] = date[1] + 1;
-        }else date[0] = date[0]+ 1;
+        if (date[1] == 1 || date[1] == 3 || date[1] == 5 || date[1] == 7 || date[1] == 8 || date[1] == 10 || date[1] == 12) {
+            if (date[0] == 31) {
+                date[0] = 1;
+                date[1] = date[1] + 1;
+            } else {
+                date[0] = date[0] + 1;
+            }
+        } else {
+            if (date[0] == 30) {
+                date[0] = 1;
+                date[1] = date[1] + 1;
+            } else {
+                date[0] = date[0] + 1;
+            }
+        }
         return date;
     }
 }
