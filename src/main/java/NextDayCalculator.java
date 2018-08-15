98fc8f2bc88255a447db01cd96d68b5bb0fad6e2
datdta95 <datdta95@gmail.com>
2018-08-15 14:32:42 +0700

    Pass với value là 1-1-2018

---
+++ src/main/java/NextDayCalculator.java
@@ -0,0 +1,10 @@
+public class NextDayCalculator {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] nextDay(int[] date) {
+        date[0] = date[0]+ 1;
+        return date;
+    }
+}
