b814e1deeda44afadfe63917c6d4305bf26b4676
datdta95 <datdta95@gmail.com>
2018-08-15 15:20:41 +0700

    Refactor

--- src/main/java/NextDayCalculator.java
+++ src/main/java/NextDayCalculator.java
@@ -4,92 +4,29 @@ public class NextDayCalculator {
     }
 
     public static int[] nextDay(int[] date) {
-        if (date[2] % 4 == 0) {
-            if (date[2] % 100 == 0) {
-                if (date[2] % 400 == 0) {
-                    if (date[1] == 1 || date[1] == 3 || date[1] == 5 || date[1] == 7 || date[1] == 8 || date[1] == 10 || date[1] == 12) {
-                        if (date[0] == 31) {
-                            date[0] = 1;
-                            date[1] = date[1] + 1;
-                        } else {
-                            date[0] = date[0] + 1;
-                        }
-
-                    } else if (date[1] == 2) {
-                        if (date[0] == 29) {
-                            date[0] = 1;
-                            date[1] = date[1] + 1;
-                        } else {
-                            date[0] = date[0] + 1;
-                        }
-                    } else {
-                        if (date[0] == 30) {
-                            date[0] = 1;
-                            date[1] = date[1] + 1;
-                        } else {
-                            date[0] = date[0] + 1;
-                        }
-                    }
+        boolean leapYear;
+        if (date[2]%4==0) {
+            if (date[2]%100==0) {
+                if (date[2]%400==0) {
+                    leapYear = true;
                 } else {
-                    if (date[1] == 1 || date[1] == 3 || date[1] == 5 || date[1] == 7 || date[1] == 8 || date[1] == 10 || date[1] == 12) {
-                        if (date[0] == 31) {
-                            date[0] = 1;
-                            date[1] = date[1] + 1;
-                        } else {
-                            date[0] = date[0] + 1;
-                        }
-
-                    } else if (date[1] == 2) {
-                        if (date[0] == 28) {
-                            date[0] = 1;
-                            date[1] = date[1] + 1;
-                        } else {
-                            date[0] = date[0] + 1;
-                        }
-                    } else {
-                        if (date[0] == 30) {
-                            date[0] = 1;
-                            date[1] = date[1] + 1;
-                        } else {
-                            date[0] = date[0] + 1;
-                        }
-                    }
+                    leapYear = false;
                 }
             } else {
-                if (date[1] == 1 || date[1] == 3 || date[1] == 5 || date[1] == 7 || date[1] == 8 || date[1] == 10 || date[1] == 12) {
-                    if (date[0] == 31) {
-                        date[0] = 1;
-                        date[1] = date[1] + 1;
-                    } else {
-                        date[0] = date[0] + 1;
-                    }
-
-                } else if (date[1] == 2) {
-                    if (date[0] == 29) {
-                        date[0] = 1;
-                        date[1] = date[1] + 1;
-                    } else {
-                        date[0] = date[0] + 1;
-                    }
-                } else {
-                    if (date[0] == 30) {
-                        date[0] = 1;
-                        date[1] = date[1] + 1;
-                    } else {
-                        date[0] = date[0] + 1;
-                    }
-                }
+                leapYear = true;
             }
-        } else {
-            if (date[1] == 1 || date[1] == 3 || date[1] == 5 || date[1] == 7 || date[1] == 8 || date[1] == 10 || date[1] == 12) {
-                if (date[0] == 31) {
-                    date[0] = 1;
-                    date[1] = date[1] + 1;
-                } else {
-                    date[0] = date[0] + 1;
-                }
+        } else leapYear = false;
 
-            } else if (date[1] == 2) {
+        if (date[1] == 1 || date[1] == 3 || date[1] == 5 || date[1] == 7 || date[1] == 8 || date[1] == 10 || date[1] == 12) {
+            if (date[0] == 31) {
+                date[0] = 1;
+                date[1] = date[1] + 1;
+            } else {
+                date[0] = date[0] + 1;
+            }
+
+        } else if (date[1] == 2) {
+            if (!leapYear) {
                 if (date[0] == 28) {
                     date[0] = 1;
                     date[1] = date[1] + 1;
@@ -97,16 +34,22 @@ public class NextDayCalculator {
                     date[0] = date[0] + 1;
                 }
             } else {
-                if (date[0] == 30) {
+                if (date[0] == 29) {
                     date[0] = 1;
                     date[1] = date[1] + 1;
                 } else {
                     date[0] = date[0] + 1;
                 }
             }
+
+        } else {
+            if (date[0] == 30) {
+                date[0] = 1;
+                date[1] = date[1] + 1;
+            } else {
+                date[0] = date[0] + 1;
+            }
         }
-
-
         return date;
     }
 }
