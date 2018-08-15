25a0688bbbf1a6d03d5beeef225e78eff67f6d7b
datdta95 <datdta95@gmail.com>
2018-08-15 14:35:30 +0700

    Test: Failed với value là 31-1-2018

--- src/test/java/NextDayCalculatorTest.java
+++ src/test/java/NextDayCalculatorTest.java
@@ -12,6 +12,14 @@ public class NextDayCalculatorTest {
         int[] actual = NextDayCalculator.nextDay(arr);
         assertArrayEquals(expect, actual);
     }
+    @Test
+    @DisplayName("Testing with value: 31-1-2018")
+    public  void nextDayWithDay31() {
+        int[] arr = {31, 1, 2018};
+        int[] expect = {1, 2, 2018};
+        int[] actual = NextDayCalculator.nextDay(arr);
+        assertArrayEquals(expect, actual);
+    }
 
 
 }
