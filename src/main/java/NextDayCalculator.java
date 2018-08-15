8a54bc7d3a29747d8845fa241f557b9cbbf7b265
datdta95 <datdta95@gmail.com>
2018-08-15 15:30:14 +0700

    Pass với value 31-12-2020

--- src/main/java/NextDayCalculator.java
+++ src/main/java/NextDayCalculator.java
@@ -17,7 +17,7 @@ public class NextDayCalculator {
             }
         } else leapYear = false;
 
-        if (date[1] == 1 || date[1] == 3 || date[1] == 5 || date[1] == 7 || date[1] == 8 || date[1] == 10 || date[1] == 12) {
+        if (date[1] == 1 || date[1] == 3 || date[1] == 5 || date[1] == 7 || date[1] == 8 || date[1] == 10) {
             if (date[0] == 31) {
                 date[0] = 1;
                 date[1] = date[1] + 1;
@@ -42,6 +42,14 @@ public class NextDayCalculator {
                 }
             }
 
+        } else if (date[1] == 12) {
+            if (date[0] == 31) {
+                date[0] = 1;
+                date[1] = 1;
+                date[2] = date[2] + 1;
+            } else {
+                date[0] = date[0] + 1;
+            }
         } else {
             if (date[0] == 30) {
                 date[0] = 1;
